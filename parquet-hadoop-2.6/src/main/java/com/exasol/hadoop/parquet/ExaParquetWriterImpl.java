4dbbcdbd3c4bbcc5c83fb4956fd890dffb5a0ea6
exazg <exazg@users.noreply.github.com>
2018-01-24 11:55:23 +0100

    Cleanup ExaParquetWriterImpl

--- parquet-hadoop-2.6/src/main/java/com/exasol/hadoop/parquet/ExaParquetWriterImpl.java
+++ parquet-hadoop-2.6/src/main/java/com/exasol/hadoop/parquet/ExaParquetWriterImpl.java
@@ -17,16 +17,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ExaParquetWriterImpl implements ExaParquetWriter {
-    private MessageType schema;
-    private int numColumns;
-    private Configuration conf;
-    private Path path;
-    private String compressionType;
-    private ExaIterator exa;
-    private int firstColumnIndex;
-    private List<Integer> dynamicPartitionExaColNums;
+
     private ParquetWriter<Tuple> writer;
     private Tuple row;
+
+    private ExaParquetWriterImpl(final MessageType schema,
+                                 final int numColumns,
+                                 final Configuration conf,
+                                 final Path path,
+                                 final String compressionType,
+                                 final ExaIterator exa,
+                                 final int firstColumnIndex,
+                                 final List<Integer> dynamicPartitionExaColNums) throws Exception {
+        System.out.println("Path: " + path.toString());
+        System.out.println("Parquet schema:\n" + schema);
+
+        TupleWriteSupport.setSchema(schema, conf);
+        this.writer = new ParquetWriter<>(path,
+                new TupleWriteSupport(),
+                CompressionCodecName.fromConf(compressionType),
+                ParquetWriter.DEFAULT_BLOCK_SIZE,
+                ParquetWriter.DEFAULT_PAGE_SIZE,
+                ParquetWriter.DEFAULT_PAGE_SIZE,
+                ParquetWriter.DEFAULT_IS_DICTIONARY_ENABLED,
+                ParquetWriter.DEFAULT_IS_VALIDATING_ENABLED,
+                conf);
+
+        // Create Tuple object with ExaIterator reference.
+        this.row = new Tuple(exa, numColumns, firstColumnIndex, dynamicPartitionExaColNums);
+    }
 
     public ExaParquetWriterImpl(final List<String> colNames,
                                 final List<TypeInfo> colTypes,
@@ -36,11 +55,14 @@ public class ExaParquetWriterImpl implements ExaParquetWriter {
                                 final ExaIterator exa,
                                 final int firstColumnIndex,
                                 final List<Integer> dynamicPartitionExaColNums) throws Exception {
-        this.schema = HiveSchemaConverter.convert(colNames, colTypes);
-        System.out.println("Parquet schema:\n" + schema);
-        this.numColumns = colNames.size();
-
-        init(conf, path, compressionType, exa, firstColumnIndex, dynamicPartitionExaColNums);
+        this(HiveSchemaConverter.convert(colNames, colTypes),
+                colNames.size(),
+                conf,
+                path,
+                compressionType,
+                exa,
+                firstColumnIndex,
+                dynamicPartitionExaColNums);
     }
 
     public ExaParquetWriterImpl(final List<ExaParquetTypeInfo> schemaTypes,
@@ -52,40 +74,14 @@ public class ExaParquetWriterImpl implements ExaParquetWriter {
                                 final List<Integer> dynamicPartitionExaColNums) throws Exception {
         // Use the schemaTypes provided since HCat table metadata isn't available.
         // This should normally only be used for testing.
-        this.schema = new MessageType("hive_schema", ExaParquetWriterImpl.typeInfoToParquetTypes(schemaTypes));
-        System.out.println("Parquet schema:\n" + schema);
-        this.numColumns = schemaTypes.size();
-
-        init(conf, path, compressionType, exa, firstColumnIndex, dynamicPartitionExaColNums);
-    }
-
-    private void init(final Configuration conf,
-                 final Path path,
-                 final String compressionType,
-                 final ExaIterator exa,
-                 final int firstColumnIndex,
-                 final List<Integer> dynamicPartitionExaColNums) throws Exception {
-        this.conf = conf;
-        this.path = path;
-        System.out.println("Path: " + path.toString());
-        this.compressionType = compressionType;
-        this.exa = exa;
-        this.firstColumnIndex = firstColumnIndex;
-        this.dynamicPartitionExaColNums = dynamicPartitionExaColNums;
-
-        TupleWriteSupport.setSchema(this.schema, this.conf);
-        this.writer = new ParquetWriter<>(this.path,
-                new TupleWriteSupport(),
-                CompressionCodecName.fromConf(this.compressionType),
-                ParquetWriter.DEFAULT_BLOCK_SIZE,
-                ParquetWriter.DEFAULT_PAGE_SIZE,
-                ParquetWriter.DEFAULT_PAGE_SIZE,
-                ParquetWriter.DEFAULT_IS_DICTIONARY_ENABLED,
-                ParquetWriter.DEFAULT_IS_VALIDATING_ENABLED,
-                this.conf);
-
-        // Create Tuple object with ExaIterator reference.
-        this.row = new Tuple(this.exa, this.numColumns, this.firstColumnIndex, this.dynamicPartitionExaColNums);
+        this(new MessageType("hive_schema", ExaParquetWriterImpl.typeInfoToParquetTypes(schemaTypes)),
+                schemaTypes.size(),
+                conf,
+                path,
+                compressionType,
+                exa,
+                firstColumnIndex,
+                dynamicPartitionExaColNums);
     }
 
     @Override
